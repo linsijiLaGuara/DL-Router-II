b57384391a8ad29f4f70993fad0c54438b49cbbe
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-02-27 16:05:13 -0300

    nuevo componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import NavbarLink from "./components/NavbarLink";
+import Home from "./views/Home";
+import Pokemon from "./views/Pokemon";
 import "./App.css";
 
 function App() {
   return (
     <>
       <NavbarLink />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Pokemon/:id" element={<Pokemon />} />
+      </Routes>
     </>
   );
 }
---
+++ src/views/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="mt-5">
+      <h1>Bienvenido maestro pokemon</h1>
+    </div>
+  );
+}
