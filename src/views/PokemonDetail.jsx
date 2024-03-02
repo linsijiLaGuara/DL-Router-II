935141587d23d9fcd3282efd58e4e5a88f6769be
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-03-01 21:00:24 -0300

    final redirecciona

--- src/views/PokemonDetail.jsx
+++ src/views/PokemonDetail.jsx
@@ -16,19 +16,9 @@ const PokemonDetail = () => {
         <>
           <Card>
             <Card.Body>
-              <div className="flex flex-col gap-3 p-3 text-align-center;">
+           
                 <h1>{pokemonDetail.name}</h1>
-                <Figure>
-                  {pokemons.image && (
-                    <Figure.Image
-                      loading="lazy"
-                      src={pokemons.image}
-                      alt={pokemonDetail.species}
-                    />
-                  )}
-                  <Figure.Caption>{pokemonDetail.gender}</Figure.Caption>
-                </Figure>
-              </div>
+              
             </Card.Body>
           </Card>
         </>
